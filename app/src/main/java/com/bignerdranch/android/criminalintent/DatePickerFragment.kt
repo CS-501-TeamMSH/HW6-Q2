f68e94e926aa18aa2ba301b4648723c109e4f1f4
md499 <mdhir@bu.edu>
2023-11-14 13:29:44 -0500

    date and time displayed

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.os.Bundle
 import android.widget.Button
 import android.widget.DatePicker
+import android.widget.ImageView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
@@ -20,30 +21,32 @@ class DatePickerFragment : DialogFragment() {
     private val args: DatePickerFragmentArgs by navArgs()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dateListener = DatePickerDialog.OnDateSetListener { _: DatePicker, year: Int, month: Int, day: Int ->
+        val dateListener = DatePickerDialog.OnDateSetListener { _, year, month, day ->
             val resultDate = GregorianCalendar(year, month, day).time
             setFragmentResult(REQUEST_KEY_DATE, bundleOf(BUNDLE_KEY_DATE to resultDate))
-
-            // Update the date
-            val callingButton = activity?.findViewById<Button>(R.id.crime_date)
-            callingButton?.text = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault()).format(resultDate)
         }
 
         val calendar = Calendar.getInstance()
         calendar.time = args.crimeDate
+
         val initialYear = calendar.get(Calendar.YEAR)
         val initialMonth = calendar.get(Calendar.MONTH)
         val initialDate = calendar.get(Calendar.DAY_OF_MONTH)
 
-        val datePickerDialog = DatePickerDialog(
+        val locale = Locale.getDefault()
+        val dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, locale)
+        val timeFormat = DateFormat.getTimeInstance(DateFormat.SHORT, locale)
+
+        return DatePickerDialog(
             requireContext(),
             dateListener,
             initialYear,
             initialMonth,
             initialDate
-        )
-
-        return datePickerDialog
+        ).apply {
+            datePicker.maxDate = Calendar.getInstance().timeInMillis
+            setTitle("${dateFormat.format(calendar.time)}")
+        }
     }
 
     companion object {
