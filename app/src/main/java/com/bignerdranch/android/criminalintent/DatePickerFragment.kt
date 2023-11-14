baad934320c9813d380f0b0a56acd88f223fb479
md499 <mdhir@bu.edu>
2023-11-14 11:48:05 -0500

    fixed issue with displaying the date

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
@@ -3,25 +3,30 @@ package com.bignerdranch.android.criminalintent
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
 import android.widget.DatePicker
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.navArgs
+import java.text.DateFormat
 import java.util.Calendar
 import java.util.GregorianCalendar
+import java.util.Locale
+import java.util.TimeZone
 
 class DatePickerFragment : DialogFragment() {
 
     private val args: DatePickerFragmentArgs by navArgs()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dateListener = DatePickerDialog.OnDateSetListener {
-                _: DatePicker, year: Int, month: Int, day: Int ->
-            
+        val dateListener = DatePickerDialog.OnDateSetListener { _: DatePicker, year: Int, month: Int, day: Int ->
             val resultDate = GregorianCalendar(year, month, day).time
-
             setFragmentResult(REQUEST_KEY_DATE, bundleOf(BUNDLE_KEY_DATE to resultDate))
+
+            // Update the date
+            val callingButton = activity?.findViewById<Button>(R.id.crime_date)
+            callingButton?.text = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault()).format(resultDate)
         }
 
         val calendar = Calendar.getInstance()
@@ -30,13 +35,15 @@ class DatePickerFragment : DialogFragment() {
         val initialMonth = calendar.get(Calendar.MONTH)
         val initialDate = calendar.get(Calendar.DAY_OF_MONTH)
 
-        return DatePickerDialog(
+        val datePickerDialog = DatePickerDialog(
             requireContext(),
             dateListener,
             initialYear,
             initialMonth,
             initialDate
         )
+
+        return datePickerDialog
     }
 
     companion object {
